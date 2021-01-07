58074dc7cccc057bc2555c89dff4791f4378920a
Hamza Rashed <hamzarashed503@gmail.com>
2021-01-07 12:01:30 -0800

    done

--- blog/views.py
+++ blog/views.py
@@ -4,25 +4,25 @@ from .models import Post
 from django.urls import reverse_lazy
 
 class BlogListView(ListView):
-    template_name = 'home.html'
+    template_name = 'base.html'
     model = Post
 
 class BlogDetailView(DetailView):
-    template_name = 'post_details.html'
+    template_name = 'blog_details.html'
     model = Post
 
 class BlogCreateView(CreateView):
-    template_name = 'post_create.html'
+    template_name = 'blog_create.html'
     model = Post
     fields = ['title', 'author', 'body']
 
 class BlogUpdateView(UpdateView):
-    template_name = 'post_update.html'
+    template_name = 'blog_update.html'
     model = Post
     fields = ['title', 'author', 'body']
 
 class BlogDeleteView(DeleteView):
-    template_name = 'post_delete.html'
+    template_name = 'blog_delete.html'
     model = Post
     fields = ['title', 'author', 'body']
     success_url = reverse_lazy('home')
